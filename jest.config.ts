4fa300f3e1057c9e0a22772d339ee4e7205fe3d9
harperllama <kyle@kyleshrives.co.uk>
2021-03-25 11:35:03 +0000

    add ignore config for jest and update README

--- jest.config.ts
+++ jest.config.ts
@@ -152,9 +152,11 @@ export default {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: [
+    "<rootDir>/dist/",
+    "<rootDir>/node_modules/",
+    "<rootDir>/__tests__/mocks/",
+  ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
